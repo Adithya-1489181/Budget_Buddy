125b251b4132c242271770af7d8036fc9917b098
Adithya1489181 <adithya1489181@gmail.com>
2024-12-24 15:07:18 +0530

    Updated db.js with variable database credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-
 const { Sequelize } = require('sequelize');
 
-// Create a new Sequelize instance with the required configuration
-const sequelize = new Sequelize('budget_buddy_2', 'root', 'Adi1489181', {
-  host: 'localhost',
-  dialect: 'mysql', // or 'postgres', 'sqlite', 'mariadb', 'mssql'
+// Load environment variables from .env file
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT, // or 'postgres', 'sqlite', 'mariadb', 'mssql'
   logging: false, // Disable logging; default: console.log
 });
 
